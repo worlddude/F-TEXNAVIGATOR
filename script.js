8409c57442f18d54e61c72f717ed27bababf248c
worlddude <88619484+worlddude@users.noreply.github.com>
2025-10-24 13:47:49 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const SUPABASE_BUCKET = 'item_images'; // change if you named it differently
 
 // ✅ Initialize Supabase client (v2 UMD build)
-const { createClient } = supabase;
+const { createClient } = Supabase;
 const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 /* -------------------------
